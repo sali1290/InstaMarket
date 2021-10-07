7c304f37c3d884178482fc935116ed183d298f09
Ali Nabavi <arz1379n@gmail.com>
2021-10-07 18:25:06 +0330

    add splash screen

--- app/src/main/java/com/e/instamarket/activity/MainActivity.kt
+++ app/src/main/java/com/e/instamarket/activity/MainActivity.kt
@@ -3,10 +3,8 @@ package com.e.instamarket.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.e.instamarket.R
-import com.e.instamarket.adapter.MenuItemData
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import github.hellocsl.cursorwheel.CursorWheelLayout
 
 
 @AndroidEntryPoint
@@ -14,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_InstaMarket)
         setContentView(R.layout.activity_main)
         findViewById<BottomNavigationView>(R.id.bottom_navigation).selectedItemId = R.id.home
     }
